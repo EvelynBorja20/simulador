e308c11dff673ddeefce038ffb3affaec00912a8
EvelynBorja20 <tatisborjalinda2018@gmail.com>
2026-04-25 08:23:16 -0500

    Funcion calcularDisponible Implementada

---
+++ funciones.js
@@ -0,0 +1,9 @@
+export function calcularDisponible(ingresos, egresos) {
+    let disponible = ingresos - egresos;
+
+    if (disponible < 0) {
+        return 0;
+    }
+
+    return disponible;
+}//AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
